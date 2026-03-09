0616138da80aecf7227252a52d743621fb31d7f7
Eddy Tucubal <etucubal-2024505@kinal.edu.gt>
2026-03-08 19:08:00 -0600

    Pruebas

--- src/AuthServiceSignTrack.Persistence/obj/Debug/net8.0/AuthServiceSignTrack.Persistence.AssemblyInfo.cs
+++ src/AuthServiceSignTrack.Persistence/obj/Debug/net8.0/AuthServiceSignTrack.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceSignTrack.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1482fcc2ec3399efb2162764c7fb30d99a7ac03a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09398520dea9b6e3b09878ed415b70f89c1c0800")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceSignTrack.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceSignTrack.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
